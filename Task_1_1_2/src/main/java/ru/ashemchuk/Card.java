8ab6ff6b24f2916006161f895d791918240dcd08
Anna Shemchuk <a.shemchuk@g.nsu.ru>
2025-09-21 16:11:57 +0700

    wip

--- Task_1_1_2/src/main/java/ru/ashemchuk/Card.java
+++ Task_1_1_2/src/main/java/ru/ashemchuk/Card.java
@@ -46,43 +46,4 @@ public class Card {
         return suit.getDisplay() + "of" + rank.getDisplay();
     }
 
-    private enum Rank {
-        TWO("Two", 2), THREE("Three", 3), FOUR("Four", 4), FIVE("Five", 5), SIX("Six", 6), SEVEN("Seven", 7), EIGHT("Eight", 8), NIGHT("Night", 9), TEN("Ten", 10), JACK("Jack", 10), QUEEN("Queen", 10), KING("King", 10), ACE("Ace", 11);
-
-        private final String display;
-        private int worth;
-
-        Rank(String display, int worth) {
-            this.display = display;
-            this.worth = worth;
-        }
-
-        public int getWorth() {
-            return worth;
-        }
-
-        public String getDisplay() { // rank or display???
-            return display;
-        }
-
-        public void downgrade() {
-            if (this == ACE) {
-                this.worth = 1;
-            }
-        }
-    }
-
-    private enum Suit {
-        SPADE("Spade"), HEART("Heart"), DIAMOND("Diamond"), CLUB("Club");
-
-        private final String name; // name ??? :(
-
-        Suit(String name) {
-            this.name = name;
-        }
-
-        public String getDisplay() {
-            return this.name;
-        }
-    }
 }
