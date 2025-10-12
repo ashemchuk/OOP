bc5c5c6b7b3149d7cb8f559ad4ab8edc9265cdfd
ashemchuk <ashemchuk@users.noreply.github.com>
2025-10-12 16:38:46 +0000

    Deploying to gh-pages from @ ashemchuk/OOP@f221877cd31a2f7f5d5d59d440b4936b41cd9d1a 🚀

--- Task_1_1_3/package-search-index.js
+++ Task_1_1_3/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.ashemchuk"},{"l":"ru.ashemchuk.expression"},{"l":"ru.ashemchuk.parser"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"ru.ashemchuk.expression"},{"l":"ru.ashemchuk.parser"}];updateSearchResults();
